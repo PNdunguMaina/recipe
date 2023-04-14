8c19d84897203248cd94f914317ddee4786ebc9f
PNdunguMaina <ndunguwamaina@gmail.com>
2023-04-14 19:35:33 +0300

    Update many-many association between food and recipe

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,7 +1,8 @@
 class Food < ApplicationRecord
     # Associations
     belongs_to :author, class_name: 'User'
-    has_many :recipe_foods, dependent: :destroy, foreign_key: true
+    has_many :recipe_foods, dependent: :destroy
+    has_many :recips, through: :recipe_foods, dependent: :destroy
 
     # Validations
     validates :name, presence: true
