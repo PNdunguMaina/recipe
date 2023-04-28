23b393a60345945905d1b841c50f45fe3d55de81
PNdunguMaina <ndunguwamaina@gmail.com>
2023-04-28 23:51:25 +0300

    Add styles to the navbar

--- app/javascript/navbar.js
+++
@@ -1,5 +0,0 @@
-// Add an event listener to the navbar-toggler button
-  document.querySelector('.navbar-toggler').addEventListener('click', function() {
-    // Toggle the .close class when the button is clicked
-    this.classList.toggle('close');
-  });
