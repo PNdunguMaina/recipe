d4a8377b61ad958d8988547d911c10a3945f446f
PNdunguMaina <ndunguwamaina@gmail.com>
2023-04-30 00:04:25 +0300

    Add functionalities for home page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,6 +12,10 @@ class ApplicationController < ActionController::Base
     foods_path
   end
 
+  def after_sign_out_path_for(resource_or_scope)
+    public_recipes_path
+  end
+
   protected
 
   def update_allowed_parameters
