77b27b47e1e50c0db8c76976befeaa84b6ae1430
PNdunguMaina <ndunguwamaina@gmail.com>
2023-04-30 00:14:23 +0300

    Fix linter errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < ActionController::Base
     foods_path
   end
 
-  def after_sign_out_path_for(resource_or_scope)
+  def after_sign_out_path_for(*)
     public_recipes_path
   end
 
